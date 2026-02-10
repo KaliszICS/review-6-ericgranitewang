7cc4d79091f760ae95985ae8fae5435424045491
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-02-10 14:34:11 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,43 @@
+import java.util.*;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static int sum (int a, int b) {
+		return a+b;
+	}
 
+	public static int difference (int a, int b) {
+		return a-b;
+	}
+	
+	public static double product (double a, double b) {
+		return a*b;
+	}
+
+	public static String removeFirst (String a) {
+		return a.substring(1);
+	}
+
+	public static int combinedLength (String a, String b) {
+		return a.length() + b.length();
+	}
+
+	public static boolean isEven (int a) {
+		return Math.abs(a%2)==0;
+	}
+
+	public static boolean isOdd (int a) {
+		return Math.abs(a%2)==1;
+	}
+
+	public static boolean isPositive (int a) {
+		return a>0;
+	}
+
+	public static boolean isNegative (int a) {
+		return a<0;
+	}
 }
